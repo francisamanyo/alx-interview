cf00278cb0b1e0afc52290d7dbdc4839f4bb0293
francisamanyo <amanyofrancis@yahoo.com>
2023-10-10 01:12:37 -0700

    all

--- 0x02-minimum_operations/0-minoperations.py
+++ 0x02-minimum_operations/0-minoperations.py
@@ -1,21 +1,21 @@
 #!/usr/bin/python3
-""" Minimum Operations
-    """
+"""In a text file, there is a single character H.
+ Your text editor can execute only two operations
+ in this file: Copy All and Paste. Given a number
+n, write a method that calculates the fewest number
+of operations needed to result in exactly n H
+characters in the file."""
 
 
 def minOperations(n: int) -> int:
-    """ Minimum Operations needed to get n H characters """
-    next = 'H'
-    body = 'H'
+    """calculates the fewest number of operations
+    needed to result in exactly n H characters
+    in the file"""
+    process = 2
     op = 0
-    while (len(body) < n):
-        if n % len(body) == 0:
-            op += 2
-            next = body
-            body += body
-        else:
-            op += 1
-            body += next
-    if len(body) != n:
-        return 0
+    while n > 1:
+        while n % process == 0:
+            op += process
+            n /= process
+        process += 1
     return op
